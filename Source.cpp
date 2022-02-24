2cdbb54d846f075a22ab8f9e26577cfc9ba3d2aa
Vectozavr <60608292+vectozavr@users.noreply.github.com>
2022-02-24 11:16:55 +0700

    refactor

--- Source.cpp
+++ Source.cpp
@@ -11,11 +11,11 @@ int main() {
     Minecraft game;
 
     // Optimal for standard monitors:
-    game.create(1280, 720);
+    //game.create(1280, 720);
     //game.create(1920, 1080, MinecraftConsts::PROJECT_NAME, true, Consts::BACKGROUND_COLOR, sf::Style::Fullscreen);
 
     // Optimal for MacBook Pro 16 display:
-    //game.create(2048, 1152);
+    game.create(2048, 1152);
     //game.create(3840, 2160);
 
     return 0;
